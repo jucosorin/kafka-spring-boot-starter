07e151cb4972a1f2725c37015767a806664b097c
Sorin Jucovschi <jucosorin@gmail.com>
2023-01-24 19:22:46 +0200

    upgrade Testcontainers kafka version 7.3.1

--- kafka-integration-test/src/main/java/com/jucosorin/kafka/intercepted/KafkaContainerInitializer.java
+++ kafka-integration-test/src/main/java/com/jucosorin/kafka/intercepted/KafkaContainerInitializer.java
@@ -5,18 +5,10 @@ import org.testcontainers.utility.DockerImageName;
 
 public class KafkaContainerInitializer extends KafkaContainer {
 
-  public static final String ARCH = "aarch64";
-
   private static DockerImageName getImageName() {
-    final String arch = System.getProperty("os.arch");
 
-    if (ARCH.equals(arch)) {
-      return DockerImageName.parse("ghcr.io/arm64-compat/confluentinc/cp-kafka:6.2.5")
-          .asCompatibleSubstituteFor("confluentinc/cp-kafka");
-    }
-    return DockerImageName.parse("confluentinc/cp-kafka:6.2.5");
+    return DockerImageName.parse("confluentinc/cp-kafka:7.3.1");
   }
-
 
   public KafkaContainerInitializer() {
     super(getImageName());
